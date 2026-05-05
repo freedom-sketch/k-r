004b16558efe28b9fa07062b2b907b1bfdb32a97
freedom <pokomisa55@gmail.com>
2026-05-05 23:51:28 +0300

    4.3

--- tasks/4.3.c
+++ tasks/4.3.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <math.h>
 
-#define MAXOP 100
-#define NUMBER '0'
-
-#define MAXVAL 100 /* максимальная глубина стека val */
-
-#define BUFSIZE 100
+#define MAXOP 100 /* максимальный размер операнда и знака */
+#define NUMBER '0' /* сигнал, что обнаружено число */
 
 int getop(char s[]);
 void push(double);
@@ -22,36 +18,48 @@ int main()
 
     while ((type = getop(s)) != EOF) {
         switch (type) {
-            case NUMBER:
-                push(atof(s));
-                break;
-            case '+':
-                push(pop() + pop());
-                break;
-            case '*':
-                push(pop() * pop());
-                break;
-            case '-':
-                op2 = pop();
-                push(pop() - op2);
-                break;
-            case '/':
-                op2 = pop();
-                if (op2 != 0.0)
-                    push(pop() / op2);
-                else
-                    printf("error: zero divisor\n");
-                break;
-            case '\n':
-                printf("\t%.8g\n", pop());
-                break;
-            default:
-                printf("error: unknown command %s\n", s);
-                break;
+        case NUMBER:
+            push(atof(s));
+            break;
+        case '+':
+            push(pop() + pop());
+            break;
+        case '*':
+            push(pop() * pop());
+            break;
+        case '-':
+            op2 = pop();
+            push(pop() - op2);
+            break;
+        case '/':
+            op2 = pop();
+            if (op2 != 0.0)
+                push(pop() / op2);
+            else
+                printf("error: zero divisor\n");
+            break;
+        case '%':
+            op2 = pop();
+            if (op2 != 0.0)
+                push(fmod(pop(), op2));
+            else
+                printf("error: zero divisor\n");
+            break;
+        case '~':
+            push(-pop());
+            break;
+        case '\n':
+            printf("answear: %.8g\n", pop());
+            break;
+        default:
+            printf("error: unknown command %s\n", s);
+            break;
         }
     }
     return 0;
 }
+
+#define MAXVAL 100 /* максимальная глубина стека val */
 
 int sp = 0; /* следующая свободная позиция в стеке */
 double val[MAXVAL]; /* стек операндов */
@@ -76,44 +84,35 @@ double pop(void)
     }
 }
 
+#include <ctype.h>
+
 int getch(void);
 void ungetch(int);
 
 /* getop: извлекает следующий операнд или знак операции */
 int getop(char s[])
 {
-    int i = 0, c, next;
+    int i, c;
 
-    while ((s[0] = c = getch()) == ' ' || c == '\t') ;
-
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
     s[1] = '\0';
-    
-    if (c == '-') {
-        next = getch();
-        if (isdigit(next) || next == '.') {
-            s[i] = '-';
-            c = next;
-        } else {
-            ungetch(next);
-            return '-';
-        }
-    }
-
     if (!isdigit(c) && c != '.')
-        return c;
-
-    if (isdigit(c))
-        while (isdigit(s[++i] = c = getch())) ;
-    if (c == '.') {
-        s[++i] = c;
-        while (isdigit(s[++i] = c = getch())) ;
-    }
-    
+        return c; /* не число */
+    i = 0;
+    if (isdigit(c)) /* накопление целой части */
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    if (c == '.') /* накопление дробной части */
+        while (isdigit(s[++i] = c = getch()))
+            ;
     s[i] = '\0';
     if (c != EOF)
         ungetch(c);
     return NUMBER;
 }
+
+#define BUFSIZE 100
 
 char buf[BUFSIZE]; /* буфер для ungetch */
 int bufp = 0; /* следующая свободная позиция в buf */
